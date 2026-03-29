6d56f55ce35258a0148129944e45ca44663161d9
Alessandro Cornara <alessandro.cornara@galileo.edu>
2026-03-28 18:10:16 -0600

    test simple vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,16 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend funcionando 🚀");
-});
-
-// 👇 ESTA ES LA CLAVE PARA VERCEL
-module.exports = (req, res) => {
-  return app(req, res);
-};
+export default function handler(req, res) {
+  res.status(200).send("Backend funcionando 🚀");
+}
