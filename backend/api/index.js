fc72ab09bdaa7978ab8c6235e3efef55b51e5551
Alessandro Cornara <alessandro.cornara@galileo.edu>
2026-03-28 18:07:54 -0600

    fix vercel handler final

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const serverless = require("serverless-http");
 const cors = require("cors");
 
 const app = express();
@@ -7,8 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Backend funcionando 🚀");
 });
 
-module.exports = serverless(app);
+// 👇 ESTA ES LA CLAVE PARA VERCEL
+module.exports = (req, res) => {
+  return app(req, res);
+};
